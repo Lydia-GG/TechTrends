7a502737d4277928130ff60c97475fdd95d2bb7e
Lydia Samir <L-Samir@hotmail.com>
2022-10-10 23:10:21 +0200

    added connection_count

--- techtrends/app.py
+++ techtrends/app.py
@@ -4,11 +4,14 @@ import sys
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+connection_count = 0
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global connection_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    connection_count += 1
     return connection
 
 # Function to get a post using its ID
@@ -85,7 +88,8 @@ def healthz():
 @app.route('/metrics')
 def metrics():
   response = app.response_class(
-      response=json.dumps({"db_connection_count": 1, "post_count": 7}),
+      response=json.dumps(
+          {"db_connection_count": connection_count, "post_count": 7}),
       status=200,
       mimetype='application/json'
   )
